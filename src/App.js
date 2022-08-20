a3045c23a4718efc37790f405f36fe66e11d3a0e
Evgeny Galitsky <e.galitsky@bk.ru>
2022-08-20 23:09:05 +0300

    moving user, caregories into redux, updating caregory map to generate from selector

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import { onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { setCurrentUser } from "./store/user/user.action";
 
 // const Shop = () => {
 //   return <h1> I am the shop page</h1>;
 // };
 
 const App = () => {
+  const dispath = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispath(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, [dispath]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -65,13 +65,8 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapShpt) => {
-    const { title, items } = docSnapShpt.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 
-  return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
